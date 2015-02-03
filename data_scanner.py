1983cc088662ac88eaaceeec7a277e44da979a9b
Todd Doughty <tdoughty1@berkeley.edu>
2015-02-03 09:42:07 -0600

    Added Main script for basic calculations

--- data_scanner.py
+++ data_scanner.py
@@ -7,6 +7,7 @@ from glob import glob
 
 # Import common modules
 from pandas import read_csv
+from numpy import sum
 
 class DataLoader(object):
     
@@ -37,4 +38,17 @@ class DataLoader(object):
             temp = basename(fName)
             name = splitext(temp)[0]
             self.__dict__[name] = read_csv(fName)
+
+# Main script
+if __name__ == '__main__':
+    
+    # Load Raw Data
+    raw_data = DataLoader('~/Workspace/Kaggle/marchmania2015/data')
+    
+    for i in range(len(raw_data.teams)):
+        _id = raw_data.teams['team_id'][i]
+        name = raw_data.teams['team_name'][i]
+        wins = sum(raw_data.regular_season_compact_results['wteam'] == _id)
+        losses = sum(raw_data.regular_season_compact_results['lteam'] == _id)
+        print name + ': ' + str(wins) + '/' + str(losses)
 
